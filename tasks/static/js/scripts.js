56eb07dd70df7db2d2c82017f6b36f2f62eada42
Antonio Silva <antoniosergio21@hotmail.com>
2022-03-19 23:34:10 -0300

    confirm task deletion

--- tasks/static/js/scripts.js
+++ tasks/static/js/scripts.js
@@ -1,1 +1,16 @@
-console.log("Funcionou");
+$(document).ready(function() {
+
+    var deleteBtn = $('.delete-btn');
+
+    $(deleteBtn).on('click', function(e) {
+
+        e.preventDefault();
+
+        var delLink = $(this).attr('href');
+        var result = confirm('Do you want to delete this task?')
+
+        if(result) {
+            window.location.href = delLink;
+        }
+    });
+});
